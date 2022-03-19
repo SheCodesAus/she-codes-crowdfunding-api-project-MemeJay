c82f9451b153119ed62cd71228fa309b8719ce09
MemeJay <94447106+MemeJay@users.noreply.github.com>
2022-03-19 10:40:11 +0800

    creating project goals and pledges

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -3,9 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('projects/', views.ProjectList.as_view()),
-    path('projects/<int:pk>/', views.ProjectDetail.as_view()),
-    path('pledges/', views.PledgeList.as_view()),
+    path('users/', views.CustomUserList.as_view()),
+    path('users/<int:pk>/', views.CustomUserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
